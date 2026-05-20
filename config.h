11770f1a062c360cf28713b76f8a2250383168e5
xiaobaiseven <1146112953@qq.com>
2026-05-20 15:55:56 +0800

    changed fonts to source font faimly

--- config.h
+++ config.h
@@ -5,13 +5,13 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Maple Mono NL NF:pixelsize=15:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=15:antialias=true:autohint=true";
 /* Spare fonts */
 static char *font2[] = {
     /*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true",
      */
     //	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true",
-    "MiSans:pixelsize=15:antialias=true:autohint=true",
+    "Source Han Sans CN Normal:pixelsize=15:antialias=true:autohint=true",
 //    "Fira Code Nerd Font Mono:pixelsize=15:antialias=true:autohint=true",
     "Noto Color Emoji:pixelsize=15:antialias=true:autohint=true",
 
